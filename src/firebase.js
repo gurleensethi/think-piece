1586e4e0fac031cfeb9fef9fd50d78dd5f63c4e2
Gurleen Sethi <sarusethi@rocketmail.com>
2019-10-29 00:48:59 -0400

    Add firebase config.

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyCqNb-ohhCxu7_LFq6osLfMcETXnwpcuBs",
+  authDomain: "quizytime.firebaseapp.com",
+  databaseURL: "https://quizytime.firebaseio.com",
+  projectId: "quizytime",
+  storageBucket: "quizytime.appspot.com",
+  messagingSenderId: "261583412798",
+  appId: "1:261583412798:web:1fe92ab474f9fb73fafb6f"
+};
+
+firebase.initializeApp(config);
+
+// Only for debugging purposes.
+window.firebase = firebase;
+
+export default firebase;
